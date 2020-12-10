f9ea5d04354cb4ce45531268ff1b43ce90003832
Genevieve Neely <genevieve.hood@gmail.com>
2020-12-09 23:54:07 -0800

    isPangram

--- src/is_pangram.js
+++ src/is_pangram.js
@@ -1,5 +1,27 @@
-const isPangram = function(text) {
 
+const ALPHABET = 'abcdefghijklmnopqrstuvwxyz'.split("")
+
+const isPangram = function(text) {
+  const letterCount = {}
+  ALPHABET.forEach((letter) => {
+    letterCount[letter] = 0;
+  });
+  
+  const reg = /[a-zA-z]/
+  const input = text.toLowerCase().split("")
+
+  input.forEach((letter) => {
+    if(letterCount[letter] === 0) {
+      letterCount[letter] += 1;
+    }
+  });
+  
+  for(let letter of ALPHABET) {
+    if(!letterCount[letter]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 module.exports = isPangram;
